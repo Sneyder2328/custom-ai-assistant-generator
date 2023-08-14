b269ae5c217b5a3f1f964f5bd2139c15aab6f63b
Sneyder Angulo <sneyder2328@gmail.com>
2023-08-14 19:30:09 -0400

    update index

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,13 @@
-#!/usr/bin/env node
 import { program } from "commander";
 import enquirer from "enquirer";
-import changeCase from "change-case";
-import inflection from "inflection";
-import _ from "lodash";
 import { runner, Logger } from "hygen"; // Make sure to refer to Hygen's documentation for proper import
 import { resolve } from "path";
 import { execSync } from "child_process";
 
 const generateAction = (args) => {
   const logger = new Logger(console.log.bind(console));
-  const templates = resolve(__dirname, ".", "_templates");
+  // NOTE: This is relative to dist, where the build result is.
+  const templates = resolve(__dirname, "..", "_templates");
   runner(args, {
     templates,
     logger,
@@ -30,25 +27,5 @@ program
   .description("Generate code based on a specific template")
   .alias("gen")
   .action(generateAction);
-//   .action(async (template) => {
-//     console.log("template=", template);
-
-//     // Prompt user for input using Enquirer
-//     const response = await enquirer.prompt([
-//       {
-//         type: "input",
-//         name: "name",
-//         message: "Enter the name of the component:",
-//       },
-//     ]);
-//     // Manipulate and transform strings using change-case, inflection, lodash
-//     // @ts-ignore
-//     const componentName = changeCase.pascalCase(response.name);
-//     const pluralComponent = inflection.pluralize(componentName);
-//     const lodashExample = _.camelCase(pluralComponent);
-//     // Generate code using Hygen
-//     // runner(['generator', template, '--name', lodashExample]);
-//     console.log(`Generated code for ${componentName}!`);
-//   });
 
 program.parse(process.argv);
